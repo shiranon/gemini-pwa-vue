9c3fcd8ff1e517f00eb39acf32a93c9d19e2c5c3
shiranon <love.frame.craft@gmail.com>
2025-09-21 22:20:41 +0900

    chore: パッケージの更新・CSPをより厳格に設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,9 +42,9 @@ export default defineNuxtConfig({
     security: {
       headers: {
         contentSecurityPolicy: {
-          'img-src': ["'self'", 'data:', 'blob:'],
-          'script-src': ["'self'", "'unsafe-inline'"],
-          'style-src': ["'self'", "'unsafe-inline'"],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'font-src': ["'self'", 'data:', 'https:'],
+          'connect-src': ["'self'", 'https://generativelanguage.googleapis.com'],
         },
       },
     },
